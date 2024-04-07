a687ebd3dbf0b230463050d1dcb2ea365e60b7fc
Akash Shrestha <aakashtha49@gmail.com>
2024-04-07 17:24:29 -0400

    minor changes

--- DishDeck/Util.swift
+++ DishDeck/Util.swift
@@ -22,13 +22,13 @@ extension UIViewController {
 extension String {
     func localized() -> String {
         if let appLanguage = DefaultManager.getAppLanguage() {
-            if appLanguage == 1 {
-                let path = Bundle.main.path(forResource: "en", ofType: "lproj")
+            if appLanguage == 2 {
+                let path = Bundle.main.path(forResource: "fr", ofType: "lproj")
                 let bundle = Bundle(path: path!)
                 return NSLocalizedString(self, tableName: "Localizable", bundle: bundle!, value: self, comment: self)
             }
         }
-        let path = Bundle.main.path(forResource: "fr", ofType: "lproj")
+        let path = Bundle.main.path(forResource: "en", ofType: "lproj")
         let bundle = Bundle(path: path!)
         return NSLocalizedString(self, tableName: "Localizable", bundle: bundle!, value: self, comment: self)
     }
